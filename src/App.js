98dd1540f8ea30fde3cb738c64178548f880e837
Ismam Fatin Zishan <50224770+Zishan23@users.noreply.github.com>
2023-08-13 23:35:29 +1000

    css update

--- src/App.js
+++ src/App.js
@@ -6,13 +6,38 @@ function App() {
   <main>
     <header>
       <a href="" className = "logo">MyBlog</a>
-    </header>
-    <nav>
+      <nav>
       <a href="">Login</a>
       <a href="">Register</a>
     </nav>
+    </header>
+    <div className="post">
+      <img src="https://techcrunch.com/wp-content/uploads/2023/08/BG3_Underdark.png?w=1390&crop=1" alt=""></img>
+      <div className = "texts">
+      <h2>Full House Battery </h2>
+      <p>lorem ipsum </p>
 
+      </div>
+    
+    </div>
+    <div className="post">
+      <img src="https://techcrunch.com/wp-content/uploads/2023/08/BG3_Underdark.png?w=1390&crop=1" alt=""></img>
+      <div className = "texts">
+      <h2>Full House Battery </h2>
+      <p>lorem ipsum </p>
 
+      </div>
+    
+    </div>
+    <div className="post">
+      <img src="https://techcrunch.com/wp-content/uploads/2023/08/BG3_Underdark.png?w=1390&crop=1" alt=""></img>
+      <div className = "texts">
+      <h2>Full House Battery </h2>
+      <p>lorem ipsum </p>
+
+      </div>
+    
+    </div>
   </main>
   );
 }
